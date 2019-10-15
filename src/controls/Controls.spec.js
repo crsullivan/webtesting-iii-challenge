449970d9a3501afba1e780555ffe3960223e9a5b
Connor R. Sullivan <crsullivan@protonmail.com>
2019-10-15 13:27:09 -0700

    updates

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,16 +1,25 @@
 // Test away!
 import React from 'react';
-// import 'jest-dom/extend-expect';
-// import * as rtl from 'react-testing-library';
-// afterEach(rtl.cleanup)
+import 'jest-dom/extend-expect';
+import * as rtl from 'react-testing-library';
+afterEach(rtl.cleanup)
 import renderer from 'react-test-renderer';
-
+import { render, fireEvent, wait } from '@testing-library/react';
 import Controls from './Controls';
 
-describe('<Controls />', () => {
-  it('should match snapshot', () => {
-    const tree = renderer.create(<Controls />).toJSON();
+describe('Controls', () => {
+     it('renders correctly', () => {
+          const wrapper = rtl.render(<Controls />)
+          expect(wrapper.baseElement).toMatchSnapshot();
+     })
+})
 
-    expect(tree).toMatchSnapshot();
-  });
-});
+test('Check for closing and locking', () => {
+    const lockMock = jest.fn();
+    const closedMock = jest.fn();
+    const { getByText } = render(<Controls toggleLocked={lockMock} toggleClosed={closedMock} />)
+    fireEvent.click(getByText(/unlock gate|lock gate/i));
+    fireEvent.click(getByText(/open gate|close gate/i));
+
+})
+
